2480810bc536b7865171651be0fd073b72758706
D-Murenzi <murenzidavid01@gmail.com>
2024-01-22 18:59:12 +0200

    changes and O notation of task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,20 +12,27 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-
+	int sorted;
+	
 	if (size < 2)
 	{
 		return;
 	}
 	for (i = 0; i < size; i++)
 	{
+		sorted = 0;
 		for (j = 0; j < size; j++)
 		{
 			if ((array[j] > array[j + 1]) && !(j + 1 >= size))
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
+				sorted++;
 			}
+		}
+		if (sorted == 0)
+		{
+			break;
 		}
 	}
 }
